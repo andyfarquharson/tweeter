c6e3e2c97273264f98d2e2fb9c383120812e4919
Andy Farquharson <andyfarquharson@gmail.com>
2022-11-09 00:14:24 +0000

    Got the character count working. It now registers how many characters are available as well as the font turning red when you go below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  console.log("Hello TWEETER!")
+  
+  
+
+$( "#tweet-text" ).on('input', function() {
+  let count = $(this).val().length;
+  let char = 140 - count;
+
+  let counter = $(this).parent('div').next('div').children('.counter');
+  counter.text(char);
+
+  console.log(`There are ${count} of characters in text area and room for ${char} more ${counter}`);
+
+  if (char < 0) {
+    counter.css('color', 'red');
+    } else {
+      counter.css('color', 'black')
+    }
+  
+
+
+
+  }); 
+  
+
+
+
+
+
+
+
+  $( "#tweet.text" ).click(function() {
+    console.log("You're in the textzone!")
+    $( ".tweet-submit" ).keypress();
+  });
+  
+  
+});
+
