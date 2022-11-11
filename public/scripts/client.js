c29c4a02d334df9384496e90d26eb830803a5ff2
Andy Farquharson <andyfarquharson@gmail.com>
2022-11-11 03:09:16 +0000

    Went through and looked for errors.  Deleted any console.logs that were used for debugging.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Evade function for safe user input
-const evade = function (str) {
+const evade = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -12,27 +12,29 @@ const evade = function (str) {
 
 // This function lays out our tweets
 const createTweetElement = data => {
-  let $tweet = (`<article class="tweet">
-  <header>
-  <div class='user'>
-    <img src="${evade(data.user.avatars)}">
-  <p>${evade(data.user.name)}</p>    
-  </div>
-  <div id='handle'>${evade(data.user.handle)}</div>
-  </header>
-  <p id='tweet'>${evade(data.content.text)}</p>
-  <footer>
-  <div>${evade(timeago.format(data.created_at))}</div>
-  <ul>
-  <i class="fa-solid fa-flag"></i>
-  <i class="fa-solid fa-retweet"></i>
-  <i class="fa-solid fa-heart"></i>
-  </ul>
-  </footer>
+  let $tweet = (`
+  <article class="tweet">
+    <header>
+      <div class='user'>
+        <img src="${evade(data.user.avatars)}">
+        <p>${evade(data.user.name)}</p>    
+      </div>
+      <div id='handle'>${evade(data.user.handle)}</div>
+    </header>
+    <p id='tweet'>${evade(data.content.text)}</p>
+    <footer>
+      <div>${evade(timeago.format(data.created_at))}</div>
+      <ul>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </ul>
+    </footer>
   </article>
   `);
   return $tweet;
 };
+
 // Grabs tweets from /tweets
 const renderTweets = data => {
   $('#tweet-container').empty();
@@ -41,7 +43,7 @@ const renderTweets = data => {
   }
 };
 
-// console.log(renderTweets(tweetData));
+// Loads the tweets on the page
 const loadTweets = () => {
   $.ajax('/tweets', {
     method: 'GET'
@@ -55,29 +57,30 @@ const loadTweets = () => {
 const submitTweet = event => {
   event.preventDefault();
   
-  if(!$('textarea').val()) {
+  if (!$('textarea').val()) {
     return $('.error-alert').text("Uh Oh: You gotta be humming about something!").slideDown();
   }
   if ($('textarea').val().length > 140) {
     return $('.error-alert').text("Uh Oh: Looks like someones a bit of a humming bird!").slideDown();
-  } 
-  console.log('Your tweets are being posted');
+  }
+
   $.ajax('/tweets', {
     method: 'POST',
     data: $('textarea').serialize(),
   })
     .then(() => {
-    loadTweets();
-  })
+      loadTweets();
+    });
 
   $('textarea').val('');
-  $('.counter').text(140)
-  
+  $('.counter').text(140);
 };
+
+// Loads initial tweets
 loadTweets();
 
 $(document).ready(function() {
-  console.log("Hello TWEETER!")
+  console.log("Hello TWEETER!");
   
   $('form.tweet-submit').on('submit', submitTweet);
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,38 +1,21 @@
 $(document).ready(function() {
-  console.log("Hello TWEETER!")
-  
-  
 
-$( "#tweet-text" ).on('input', function() {
   let count = $(this).val().length;
   let char = 140 - count;
+  // character counter for the text box
+  $("#tweet-text").on('input', function() {
+    let counter = $(this).parent('div').next('div').children('.counter');
+    counter.text(char);
 
-  let counter = $(this).parent('div').next('div').children('.counter');
-  counter.text(char);
-
-  if (char < 0) {
-    counter.css('color', 'red');
+    if (char < 0) {
+      counter.css('color', 'red');
     } else {
       counter.css('color', 'black');
     }
-  
-
-
-
-  }); 
-  
-
-
-
-
-
-
-
-  $( "#tweet.text" ).click(function() {
-    console.log("You're in the textzone!")
-    $( ".tweet-submit" ).keypress();
   });
-  
-  
+  // Registers the submit button
+  $("#tweet.text").click(function() {
+    $(".tweet-submit").keypress();
+  });
 });
 
